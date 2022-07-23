88d71ff47272966a0bc4b83125ddb0595c7e078f
SinaMAmini <smobasheramini@gmail.com>
2022-07-23 20:37:18 +0430

    [FIX] product flex issue

--- src/app/components/header/search-box/search-box.component.ts
+++ src/app/components/header/search-box/search-box.component.ts
@@ -1,8 +1,12 @@
 import {Component} from '@angular/core';
 
+import {NgxPopperjsPlacements} from 'ngx-popperjs';
+
 @Component({
     selector: 'app-search-box',
     templateUrl: './search-box.component.html',
     styleUrls: ['./search-box.component.scss'],
 })
-export class SearchBoxComponent {}
+export class SearchBoxComponent {
+    public NgxPopperjsPlacements = NgxPopperjsPlacements;
+}
