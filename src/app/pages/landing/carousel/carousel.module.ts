3690f8cdf422488aa22b3bf3f4072f4184862a1e
SinaMAmini <smobasheramini@gmail.com>
2022-07-24 12:22:19 +0430

    [FIX] team06 review(header issues)

--- src/app/pages/landing/carousel/carousel.module.ts
+++ src/app/pages/landing/carousel/carousel.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CarouselComponent} from './carousel.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
     declarations: [CarouselComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule],
     exports: [CarouselComponent],
 })
 export class CarouselModule {}
