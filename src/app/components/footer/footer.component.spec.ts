28031e3f90e0fc4686434c28f7a03e9a2a0dcc1b
SinaMAmini <smobasheramini@gmail.com>
2022-08-01 16:18:47 +0430

    [ADD] some tests

--- src/app/components/footer/footer.component.spec.ts
+++ src/app/components/footer/footer.component.spec.ts
@@ -1,10 +1,12 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {FooterComponent} from './footer.component';
+import {host} from '@angular-devkit/build-angular/src/test-utils';
 
 describe('FooterComponent', () => {
     let component: FooterComponent;
     let fixture: ComponentFixture<FooterComponent>;
+    let host: HTMLElement;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -16,9 +18,28 @@ describe('FooterComponent', () => {
         fixture = TestBed.createComponent(FooterComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
+        host = fixture.nativeElement as HTMLElement;
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should have footer tag', () => {
+        const footerTag = host.querySelector('footer');
+
+        expect(footerTag).toBeTruthy();
+    });
+
+    it('should have footer class', () => {
+        const footerTag = host.querySelector('footer');
+
+        expect(footerTag?.className).toEqual('footer');
+    });
+
+    it('should have footer__note', () => {
+        const footer__note = host.querySelector('.footer__note');
+
+        expect(footer__note).toBeTruthy();
     });
 });
