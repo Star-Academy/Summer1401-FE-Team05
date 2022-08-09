5801dd1ac0c8b11e3cdef1054c3122b21d5bd7f8
HessamCheraghi <hessam.ch.rose@gmail.com>
2022-08-09 08:41:46 +0430

    [ADD] carousel

--- src/app/pages/landing/carousel/carousel.component.ts
+++ src/app/pages/landing/carousel/carousel.component.ts
@@ -1,8 +1,68 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
+import {Slide} from './models/slide';
+import {slides} from './slides';
 
 @Component({
     selector: 'app-carousel',
     templateUrl: './carousel.component.html',
     styleUrls: ['./carousel.component.scss'],
 })
-export class CarouselComponent {}
+export class CarouselComponent implements AfterViewInit, OnDestroy {
+    @ViewChild('scrollCards') private scrollCards!: ElementRef<HTMLElement>;
+
+    private scrollWidth: number = this.scrollCards?.nativeElement.clientWidth ?? 1200;
+
+    private readonly INTERVAL_DELAY: number = 3000;
+
+    private interval!: number;
+    private paused: boolean = false;
+
+    private slide(scrollTo: number): void {
+        if (this.activeScroll >= slides.length && scrollTo === -1) {
+            this.scrollCards.nativeElement.scrollTo(-1, 0);
+            this.activeScroll = 1;
+        } else if (this.activeScroll <= 1 && scrollTo === 1) {
+            this.scrollCards.nativeElement.scrollTo(-1 * this.scrollWidth * slides.length, 0);
+            this.activeScroll = slides.length;
+        } else {
+            this.scrollCards.nativeElement.scrollBy(scrollTo * this.scrollWidth, 0);
+            this.activeScroll -= scrollTo;
+        }
+        console.log(this.activeScroll);
+    }
+    private resetInterval(): void {
+        if (this.paused === false) {
+            clearInterval(this.interval);
+            this.interval = setInterval(() => {
+                this.slide(-1);
+            }, this.INTERVAL_DELAY);
+        } else {
+            clearInterval(this.interval);
+        }
+    }
+    public slides: Slide[] = slides;
+
+    public next(): void {
+        this.slide(-1);
+    }
+    public prev(): void {
+        this.slide(+1);
+    }
+    public activeScroll: number = 1;
+
+    public ngAfterViewInit(): void {
+        this.resetInterval();
+    }
+
+    public ngOnDestroy(): void {
+        clearInterval(this.interval);
+    }
+    public stopSliding(): void {
+        this.paused = true;
+        this.resetInterval();
+    }
+    public startSliding(): void {
+        this.paused = false;
+        this.resetInterval();
+    }
+}
---
+++ src/app/pages/landing/carousel/slides.ts
@@ -0,0 +1,54 @@
+import {Slide} from './models/slide';
+
+export const slides: Slide[] = [
+    {
+        id: 1,
+        title: 'dying light 2 stay human',
+        alt: 'dying light 2 stay human banner',
+        overlayPosition: 'bottom',
+        imgSrc: 'assets/images/banners/dying-light-2-stay-human.jpg',
+        smallText: 'lorem ipsum',
+        description:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium praesentium nobis, sint maiores assumenda aperiam!',
+    },
+    {
+        id: 2,
+        title: 'elden-ring',
+        alt: 'elden-ring banner',
+        overlayPosition: 'bottom',
+        imgSrc: 'assets/images/banners/elden-ring.webp',
+        smallText: 'lorem ipsum',
+        description:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium praesentium nobis, sint maiores assumenda aperiam!',
+    },
+    {
+        id: 3,
+        title: 'horizen-forbidden west',
+        alt: 'horizen-forbidden west banner',
+        overlayPosition: 'bottom',
+        imgSrc: 'assets/images/banners/horizen-forbidden-west.webp',
+        smallText: 'lorem ipsum',
+        description:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium praesentium nobis, sint maiores assumenda aperiam!',
+    },
+    {
+        id: 4,
+        title: 'LEGO startwars the skywalker saga',
+        alt: 'LEGO startwars the skywalker saga banner',
+        overlayPosition: 'bottom',
+        imgSrc: 'assets/images/banners/LEGO-startwars-the-skywalker-saga.jpg',
+        smallText: 'lorem ipsum',
+        description:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium praesentium nobis, sint maiores assumenda aperiam!',
+    },
+    {
+        id: 5,
+        title: 'COD MW II',
+        alt: 'COD MW II banner',
+        overlayPosition: 'bottom',
+        imgSrc: 'assets/images/banners/COD-MW-II.jpg',
+        smallText: 'lorem ipsum',
+        description:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium praesentium nobis, sint maiores assumenda aperiam!',
+    },
+];
