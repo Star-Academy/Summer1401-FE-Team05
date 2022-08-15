865d479e404945daea12d95c697215b53d4e9766
SinaMAmini <smobasheramini@gmail.com>
2022-08-15 16:59:39 +0430

    [FIX] prettier issues

--- src/app/components/row-card/row-card.module.ts
+++ src/app/components/row-card/row-card.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RowCardComponent} from './row-card.component';
-import {RouterModule} from "@angular/router";
+import {RouterModule} from '@angular/router';
 
 @NgModule({
     declarations: [RowCardComponent],
